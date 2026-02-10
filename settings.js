d9ab88680d1ffc7e810f3843657c3c2c9c9c5e04
coeroy24-svg <coeroy24@gmail.com>
2026-02-10 12:58:18 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner'Trippy', // Your name
-  ownerNumber: '233201203860', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '233591443786', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   maxStoreMessages: 20, 
