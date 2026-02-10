a40eaf37847115cad0bdd130eba5f40fce944eb6
coeroy24-svg <coeroy24@gmail.com>
2026-02-10 12:22:42 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-'Trippy' settings = {
+const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwne'Trippy', // Your name
+  botOwner'Trippy', // Your name
   ownerNumber: '233201203860', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
